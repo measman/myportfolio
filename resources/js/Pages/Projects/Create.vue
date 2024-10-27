abcf02d4d628ca3691548a63491499b497437938
Suman <tdkasman2@gmail.com>
2024-10-27 22:19:25 +0545

    create view and controlle of both projects and skills

--- resources/js/Pages/Projects/Create.vue
+++ resources/js/Pages/Projects/Create.vue
@@ -1,6 +1,25 @@
 <script setup>
 import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout.vue";
-import { Head } from "@inertiajs/vue3";
+import InputError from "@/Components/InputError.vue";
+import InputLabel from "@/Components/InputLabel.vue";
+import PrimaryButton from "@/Components/PrimaryButton.vue";
+import TextInput from "@/Components/TextInput.vue";
+import { Head, useForm } from "@inertiajs/vue3";
+
+defineProps({
+    skills: Array,
+});
+
+const form = useForm({
+    name: "",
+    image: null,
+    skill_id: "",
+    project_url: "",
+});
+
+const submit = () => {
+    form.post(route("projects.store"));
+};
 </script>
 <template>
     <Head title="New Project" />
@@ -14,7 +33,79 @@ import { Head } from "@inertiajs/vue3";
             </h2>
         </template>
         <div class="py-12">
-            <div class="mx-auto max-w-7xl sm:px-6 lg:px-8">Form</div>
+            <div class="max-w-md mx-auto sm:px-6 lg:px-8 bg-white">
+                <form class="p-4" @submit.prevent="submit">
+                    <div>
+                        <InputLabel for="skill_id" value="Skill" />
+                        <select
+                            v-model="form.skill_id"
+                            id="skill_id"
+                            name="skill_id"
+                            class="mt-1 block w-full pl-3 pr-10 py-2 text-base border-gray-300 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm rounded-md"
+                        >
+                            <option
+                                v-for="skill in skills"
+                                :key="skill.id"
+                                :value="skill.id"
+                            >
+                                {{ skill.name }}
+                            </option>
+                        </select>
+                    </div>
+                    <div>
+                        <InputLabel for="name" value="Name" />
+
+                        <TextInput
+                            id="name"
+                            type="text"
+                            class="mt-1 block w-full"
+                            v-model="form.name"
+                            autofocus
+                            autocomplete="name"
+                        />
+
+                        <InputError class="mt-2" :message="form.errors.name" />
+                    </div>
+                    <div>
+                        <InputLabel for="project_url" value="URL" />
+
+                        <TextInput
+                            id="project_url"
+                            type="text"
+                            class="mt-1 block w-full"
+                            v-model="form.project_url"
+                            autocomplete="projecturl"
+                        />
+
+                        <InputError
+                            class="mt-2"
+                            :message="form.errors.project_url"
+                        />
+                    </div>
+                    <div class="mt-2">
+                        <InputLabel for="image" value="Image" />
+
+                        <TextInput
+                            id="image"
+                            type="file"
+                            class="mt-1 block w-full"
+                            @input="form.image = $event.target.files[0]"
+                        />
+
+                        <InputError class="mt-2" :message="form.errors.image" />
+                    </div>
+
+                    <div class="mt-4 flex items-center justify-end">
+                        <PrimaryButton
+                            class="ms-4"
+                            :class="{ 'opacity-25': form.processing }"
+                            :disabled="form.processing"
+                        >
+                            Save
+                        </PrimaryButton>
+                    </div>
+                </form>
+            </div>
         </div>
     </AuthenticatedLayout>
 </template>
